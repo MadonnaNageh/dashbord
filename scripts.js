ab5d2fde67773ab9e989ae9b75aef7ff32dd6136
madonnanageh675@gmail.com <madonnanageh675@gmail.com>
2024-08-24 17:59:11 +0200

    dash

---
+++ scripts.js
@@ -0,0 +1,11 @@
+Add event listeners to sidebar buttons
+document.getElementById("dashboardBtn").addEventListener("click", function() {
+    alert("Dashboard button clicked!");
+});
+
+document.getElementById("customersBtn
+
+
+
+
+    
